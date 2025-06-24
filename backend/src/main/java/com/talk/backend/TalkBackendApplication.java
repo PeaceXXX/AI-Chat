3f098f4b0f70ff3249d63b81dc86b9178118f15d
rihuita <rtanpeace3@gmail.com>
2025-06-23 22:12:52 -0700

    back end

---
+++ backend/src/main/java/com/talk/backend/TalkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.talk.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/talk/backend/TalkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
